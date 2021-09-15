c49c8f73bb43b6a670597ebf2c59ab3c816fe2e2
Joe Timmins <joe@tickr.co.uk>
2021-09-15 17:18:58 +0100

    Add tests and implementation for search repository

--- app/src/main/java/com/joetimmins/watchbox/model/ContentSearchRepository.kt
+++ app/src/main/java/com/joetimmins/watchbox/model/ContentSearchRepository.kt
@@ -1,4 +1,98 @@
 package com.joetimmins.watchbox.model
 
-class ContentSearchRepository {
+import com.joetimmins.watchbox.model.http.OmdbContentSearchClient
+import com.joetimmins.watchbox.util.InjectableDispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.withContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ContentSearchRepository(
+    private val searchClient: OmdbContentSearchClient,
+    private val dispatchers: InjectableDispatchers
+) {
+    private val searchInput =
+        MutableStateFlow(SearchInput(term = "", contentType = ContentType.Movie))
+
+    val searchResults: Flow<ContentSearchResults> = searchInput
+        .filter { it.term.length > 2 }
+        .mapLatest { searchFor(it) }
+        .map { apiSearchResults -> apiSearchResults.toContentSearchResults() }
+        .onStart { emit(ContentSearchResults.NoSearchPerformed) }
+
+    private suspend fun searchFor(input: SearchInput) = withContext(dispatchers.io) {
+        runCatching {
+            val search = searchClient.search(
+                searchTerm = input.term,
+                contentType = input.contentType.rawContentType
+            )
+            search
+        }
+    }
+
+    fun search(term: String, contentType: ContentType) {
+        searchInput.tryEmit(SearchInput(term, contentType))
+    }
 }
+
+private data class SearchInput(
+    val term: String,
+    val contentType: ContentType
+)
+
+enum class ContentType(val rawContentType: String) {
+    Movie("movie"),
+    Series("series");
+
+    companion object {
+        fun fromRawValue(rawValue: String): ContentType = values().first {
+            it.rawContentType == rawValue.lowercase()
+        }
+    }
+}
+
+sealed interface AirDate {
+    data class Single(val yearReleased: Int) : AirDate
+    data class Ongoing(val yearFirstShown: Int, val yearLastShown: Int?) : AirDate
+
+}
+
+fun String.toAirDate(): AirDate {
+    val s = split('–') // believe it or not ... this is *not* a regular dash -
+    return when (s.size) {
+        1 -> AirDate.Single(s.first().toInt())
+        2 -> AirDate.Ongoing(s.first().toInt(), s.last().toIntOrNull())
+        else -> throw IllegalArgumentException("Invalid year range.")
+    }
+}
+
+sealed interface ContentSearchResults {
+    object NoSearchPerformed : ContentSearchResults
+    object Failure : ContentSearchResults
+    data class Success(val results: List<ContentSearchResult>) : ContentSearchResults
+}
+
+data class ContentSearchResult(
+    val title: String,
+    val imdbId: String,
+    val contentType: ContentType,
+    val airDate: AirDate,
+    val posterUrl: String,
+)
+
+private fun Result<ApiSearchResults>.toContentSearchResults(): ContentSearchResults =
+    map { it.toSuccessState() }.getOrDefault(ContentSearchResults.Failure)
+
+private fun ApiSearchResults.toSuccessState() = ContentSearchResults.Success(
+    results.map {
+        it.toContentSearchResult()
+    }
+)
+
+private fun ApiSearchResult.toContentSearchResult() = ContentSearchResult(
+    title = title,
+    imdbId = imdbId,
+    posterUrl = posterUrl,
+    contentType = ContentType.fromRawValue(contentType),
+    airDate = yearRange.toAirDate()
+)
--- app/src/test/java/com/joetimmins/watchbox/model/ContentSearchRepositoryTest.kt
+++ app/src/test/java/com/joetimmins/watchbox/model/ContentSearchRepositoryTest.kt
@@ -1,7 +1,118 @@
 package com.joetimmins.watchbox.model
 
-import org.junit.Assert.*
+import app.cash.turbine.Event
+import app.cash.turbine.test
+import com.joetimmins.watchbox.CoroutineTestRule
+import com.joetimmins.watchbox.model.http.OmdbContentSearchClient
+import com.joetimmins.watchbox.model.http.twinPeaksApiSearchResults
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 class ContentSearchRepositoryTest {
+    @get:Rule
+    val rule = CoroutineTestRule()
 
+    @Test
+    fun `when collecting results flow, and search term is not entered, result is no search performed`() {
+        val repository = contentSearchRepository(successfulSearchClient)
+
+        runBlocking {
+            repository.searchResults.test {
+                val allEmissions = cancelAndConsumeRemainingEvents()
+                    .filterIsInstance<Event.Item<ContentSearchResults>>()
+                    .map { it.value }
+                assertEquals(1, allEmissions.size)
+                assertEquals(ContentSearchResults.NoSearchPerformed, allEmissions.first())
+            }
+        }
+    }
+
+    @Test
+    fun `given search call succeeds, when collecting results flow, and search term less than three characters long is entered, result is no search performed`() {
+        val repository = contentSearchRepository(successfulSearchClient)
+
+        runBlocking {
+            repository.searchResults.test {
+                repository.search(term = "tw", contentType = ContentType.Series)
+                val allEmissions = cancelAndConsumeRemainingEvents()
+                    .filterIsInstance<Event.Item<ContentSearchResults>>()
+                    .map { it.value }
+                assertEquals(1, allEmissions.size)
+                assertEquals(ContentSearchResults.NoSearchPerformed, allEmissions.first())
+            }
+        }
+    }
+
+    @Test
+    fun `given search call succeeds, when collecting results flow, and search term three characters or longer is entered, search results are emitted`() {
+        val repository = contentSearchRepository(successfulSearchClient)
+
+        runBlocking {
+            repository.searchResults.test {
+                repository.search(term = "twin", contentType = ContentType.Series)
+                val allEmissions = cancelAndConsumeRemainingEvents()
+                    .filterIsInstance<Event.Item<ContentSearchResults>>()
+                    .map { it.value }
+                assertEquals(
+                    ContentSearchResults.Success(listOf(twinPeaksContentSearchResult)),
+                    allEmissions.last()
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `given search call fails, when collecting results flow, and search term is entered, failure state is emitted`() {
+        val repository = contentSearchRepository(failingSearchClient)
+
+        runBlocking {
+            repository.searchResults.test {
+                repository.search(term = "twin", contentType = ContentType.Series)
+                val allEmissions = cancelAndConsumeRemainingEvents()
+                    .filterIsInstance<Event.Item<ContentSearchResults>>()
+                    .map { it.value }
+                assertEquals(
+                    ContentSearchResults.Failure,
+                    allEmissions.last()
+                )
+            }
+        }
+    }
+
+    private fun contentSearchRepository(
+        searchClient: OmdbContentSearchClient
+    ) = ContentSearchRepository(
+        searchClient = searchClient,
+        dispatchers = rule.testDispatchers
+    )
 }
+
+private val successfulSearchClient = object : OmdbContentSearchClient {
+    override suspend fun search(
+        searchTerm: String,
+        contentType: String,
+        apiKey: String
+    ) = twinPeaksApiSearchResults
+}
+
+private val failingSearchClient = object : OmdbContentSearchClient {
+    override suspend fun search(
+        searchTerm: String,
+        contentType: String,
+        apiKey: String
+    ): ApiSearchResults {
+        throw Throwable()
+    }
+}
+
+private val twinPeaksContentSearchResult = ContentSearchResult(
+    title = "Twin Peaks",
+    contentType = ContentType.Series,
+    airDate = AirDate.Ongoing(1990, 1991),
+    imdbId = "tt0098936",
+    posterUrl = "https://m.media-amazon.com/images/M/MV5BMTExNzk2NjcxNTNeQTJeQWpwZ15BbWU4MDcxOTczOTIx._V1_SX300.jpg",
+)
