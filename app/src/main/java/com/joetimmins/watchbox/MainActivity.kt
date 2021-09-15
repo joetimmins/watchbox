b28d21b30d9d80df6a789245a694c4502f08bc44
Joe Timmins <joe@tickr.co.uk>
2021-09-15 22:59:21 +0100

    Add internet permission 😅

--- app/src/main/java/com/joetimmins/watchbox/MainActivity.kt
+++ app/src/main/java/com/joetimmins/watchbox/MainActivity.kt
@@ -3,11 +3,7 @@ package com.joetimmins.watchbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.joetimmins.watchbox.ui.search.SearchScreenContent
 import com.joetimmins.watchbox.ui.theme.WatchboxTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WatchboxTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                SearchScreenContent()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WatchboxTheme {
-        Greeting("Android")
     }
 }
